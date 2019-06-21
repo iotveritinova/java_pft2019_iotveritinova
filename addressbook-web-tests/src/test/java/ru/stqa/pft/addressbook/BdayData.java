5f685237134b28eed9abc3fe91bc2168c86599b9
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-06-21 15:16:14 +0400

    Revert "№5 Создан ApplicationManager, наследование заменено на делегирование,"
    This reverts commit 6a9889797b74656cd1b66f5d7b8dbae8c7a5a57f.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/BdayData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/BdayData.java
@@ -1,11 +1,11 @@
-package ru.stqa.pft.addressbook.model;
+package ru.stqa.pft.addressbook;
 
-public class AdayData {
+public class BdayData {
   private final String day;
   private final String month;
   private final String year;
 
-  public AdayData(String day, String month, String year) {
+  public BdayData(String day, String month, String year) {
     this.day = day;
     this.month = month;
     this.year = year;
