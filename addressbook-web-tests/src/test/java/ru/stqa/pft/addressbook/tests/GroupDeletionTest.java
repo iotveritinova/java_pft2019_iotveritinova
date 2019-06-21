2febdbd0e300f0e8a154f57f4d27a5ad2c2576b8
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-06-21 15:16:26 +0400

    Revert "Revert "№5 Создан ApplicationManager, наследование заменено на делегирование,""
    This reverts commit 5f685237134b28eed9abc3fe91bc2168c86599b9.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.tests;
+
+
+
+import org.testng.annotations.*;
+
+public class GroupDeletionTest extends TestBase {
+  @Test
+  public void testGroupDeletion() throws Exception {
+
+    app.gotoGroupPage();
+    app.selectGroup();
+    app.deleteSelectegGroups();
+    app.returnToGroupPage();
+
+  }
+
+
+}
