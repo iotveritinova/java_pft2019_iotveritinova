cae3ff446abb2c3bf7d838cdd105d616036e187a
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-06-21 16:57:06 +0400

    №5 добавлен BaseHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/app/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/app/ApplicationManager.java
@@ -1,8 +1,6 @@
 package ru.stqa.pft.addressbook.app;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
@@ -33,23 +31,7 @@ public class ApplicationManager {
     wd.quit();
   }
 
-  public boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
 
-  public boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
 
   public void returnToHomePage() {
     wd.findElement(By.linkText("home")).click();
