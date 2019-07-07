25c7d5e5885e648f38c47e27c2159565053fe277
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-07-07 10:59:43 +0400

    #6 ContactDeletionTest created

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
@@ -1,45 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class ContactDeletionTest extends TestBase {
-
-  private WebDriver wd;
-  private boolean acceptNextAlert = true;
-
   @Test
   public void testContactDeletion() throws Exception {
     app.getNavigationHelper().gotoContactPage();
     app.getContactHelper().selectContact();
+    app.getContactHelper().deleteSelectedContacts();
+    app.getNavigationHelper().isAlertPresent();
+    app.getNavigationHelper().acceptAlert();
 
-    //acceptNextAlert = true;
-    app.getContactHelper().deleteSelectedContacts();    app.getContactHelper().selectContact();
-    acceptAlert();
+
     app.getContactHelper().returnToHomePage();
   }
-
- // private void acceptAlert() {
-   // Alert alert = wd.switchTo().alert();
-    //alert.accept();
-  //}
-  private String acceptAlert() {
-    try {
-      Alert alert = wd.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
-
 }
 
 
