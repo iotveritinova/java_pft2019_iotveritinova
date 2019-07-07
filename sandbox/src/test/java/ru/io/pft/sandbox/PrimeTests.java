2e837bc3c37a4040a5d733483cceb7b28b5d0b2a
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-07-07 20:58:56 +0400

    Лекция 4.1 PrimeTests ускорены

--- sandbox/src/test/java/ru/io/pft/sandbox/PrimeTests.java
+++ sandbox/src/test/java/ru/io/pft/sandbox/PrimeTests.java
@@ -6,11 +6,33 @@ import org.testng.annotations.Test;
 public class PrimeTests {
   @Test
   public void testPrime() {
-    Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
   }
 
   @Test
   public void testNonPrime() {
-    Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE-2));
+    Assert.assertFalse(Primes.isPrimeFast(Integer.MAX_VALUE - 2));
+  }
+
+  @Test (enabled = false)
+  public void testPrimeWhile() {
+    Assert.assertTrue(Primes.isPrimeWhile(Integer.MAX_VALUE));
+  }
+
+  @Test(enabled = false)
+  public void testNonPrimeWhile() {
+    Assert.assertFalse(Primes.isPrimeWhile(Integer.MAX_VALUE - 2));
+  }
+
+  @Test(enabled = false)
+  public void testPrimeLong() {
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
+  }
+
+  @Test(enabled = false)
+  public void testNonPrimeLong() {
+    long n = Integer.MAX_VALUE - 2;
+    Assert.assertFalse(Primes.isPrime(n));
   }
 }
