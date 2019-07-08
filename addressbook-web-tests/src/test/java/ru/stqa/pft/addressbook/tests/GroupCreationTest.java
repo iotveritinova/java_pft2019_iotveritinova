4ffcacc230b772dc88d642a6d8cf935dddd492c1
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-07-08 23:19:10 +0400

    Лекция 4.9 GroupCreationTest вычисление макс идентификатора заменено на лямбда-выражение

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -1,10 +1,9 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -25,20 +24,7 @@ public class GroupCreationTest extends TestBase {
     Assert.assertEquals(after.size(), before.size() + 1);
     //проверка списков с записями
     before.add(group);
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    Comparator<? super GroupData> byId=new Comparator<GroupData>() {
-      @Override
-      public int compare(GroupData o1, GroupData o2) {
-        return Integer.compare(o1.getId(),o2.getId());
-      }
-    };
-    int max1=after.stream().max(byId).get().getId();
-    group.setId(max1);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
