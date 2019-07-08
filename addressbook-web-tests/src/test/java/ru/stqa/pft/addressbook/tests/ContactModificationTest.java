894ba940367c6d09c8cc9946cc34cdf5793f9628
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-07-09 01:10:44 +0400

    №9 ContactModificationTest добавлено удаление последнего элемента из списка , доп комит

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
@@ -1,8 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.List;
 
 public class ContactModificationTest extends TestBase {
   @Test
@@ -11,11 +14,16 @@ public class ContactModificationTest extends TestBase {
     if (!app.getContactHelper().isThereAContract()) {
       app.getContactHelper().createContract(new ContactData("Petr", "Petrovich", "Petrov", "Sadovaya st.,2,204b", "999-999", "8-999-111-22-33", "email@mail.ru", "[none]"), true);
     }
-    app.getContactHelper().selectContact();
+    List<ContactData> before = app.getContactHelper().getContactList();
+    app.getContactHelper().selectContact(before.size()-1);
     app.getContactHelper().editSelectedContacts();
-    app.getContactHelper().fillContactData(new ContactData("changedName", "changedPatronim", "changedSurname", "new address", "888-000-000", "777-77-22-22", "newEmail@mail.ru", null), false);
+    ContactData contact = new ContactData("changedName", "changedPatronim", "changedSurname", "new address", "888-000-000", "777-77-22-22", "newEmail@mail.ru", null);
+    app.getContactHelper().fillContactData(contact, false);
     app.getContactHelper().submitUpdateContact();
     app.getContactHelper().returnToHomePage();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    //проверка количества записей
+    Assert.assertEquals(after.size(), before.size());
   }
 
 }
