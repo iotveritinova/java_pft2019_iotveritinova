3456619b315130b79b73fd79ba749613154b091a
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-07-08 23:10:57 +0400

    Лекция 4.9 GroupCreationTest добавлен Comparator

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -30,7 +31,14 @@ public class GroupCreationTest extends TestBase {
         max = g.getId();
       }
     }
-    group.setId(max);
+    Comparator<? super GroupData> byId=new Comparator<GroupData>() {
+      @Override
+      public int compare(GroupData o1, GroupData o2) {
+        return Integer.compare(o1.getId(),o2.getId());
+      }
+    };
+    int max1=after.stream().max(byId).get().getId();
+    group.setId(max1);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
