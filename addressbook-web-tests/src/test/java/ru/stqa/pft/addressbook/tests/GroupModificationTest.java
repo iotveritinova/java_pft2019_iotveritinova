bee1404a8e50d35c4d082227ac34a70067c42bc5
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-07-08 23:30:27 +0400

    Лекция 4.10 GroupModificationTest добавлена сортировка списков перед сравнением

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -1,10 +1,10 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupModificationTest extends TestBase {
@@ -19,7 +19,7 @@ public class GroupModificationTest extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupUpdate();
-    GroupData group = new GroupData(before.get(before.size() - 1).getId(),"changedGroup1", "test2", "test3");
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "changedGroup1", "test2", "test3");
     app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupUpdate();
     app.getGroupHelper().returnToGroupPage();
@@ -27,9 +27,14 @@ public class GroupModificationTest extends TestBase {
     //проверка количества записей
     Assert.assertEquals(after.size(), before.size());
     //проверка списков с записями
-    before.remove(before.size()-1);
+    before.remove(before.size() - 1);
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    //сортировка списка
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    //сравнение списков
+    Assert.assertEquals(before, after);
   }
 
 }
