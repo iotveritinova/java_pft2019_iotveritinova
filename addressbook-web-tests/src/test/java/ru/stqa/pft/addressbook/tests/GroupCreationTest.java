c5c8197edca0cc7b8cd09b0e8cb8608c71fe40fb
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-07-08 23:35:38 +0400

    Лекция 4.10 GroupCreationTest добавлена сортировка списков перед сравнением

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupCreationTest extends TestBase {
@@ -24,8 +24,13 @@ public class GroupCreationTest extends TestBase {
     Assert.assertEquals(after.size(), before.size() + 1);
     //проверка списков с записями
     before.add(group);
-    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
+    //сортировка списка
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    //сравнение списков
+    Assert.assertEquals(before, after);
   }
 
 }
