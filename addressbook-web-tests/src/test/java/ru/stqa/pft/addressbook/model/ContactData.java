095438c5fe8a70d86b54d0431844644f009927f2
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-07-11 04:37:08 +0400

    №9 ContactData заново сгенерированы методы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -14,24 +14,6 @@ public class ContactData {
   private String group;
 
 
-  @Override
-  public String toString() {
-    return "ContactData{" + "id=" + id + ", firstName='" + firstName + '\'' + '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    ContactData that = (ContactData) o;
-    return id == that.id && Objects.equals(firstName, that.firstName);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, firstName);
-  }
-
   public ContactData(int id, String firstName, String middleName, String lastName, String address, String homePhone, String mobilePhone, String email1, String group) {
     this.id = id;
     this.firstName = firstName;
@@ -94,5 +76,23 @@ public class ContactData {
 
   public void setId(int id) {
     this.id = id;
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" + "id=" + id + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ContactData that = (ContactData) o;
+    return id == that.id && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, firstName, lastName);
   }
 }
