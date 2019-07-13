552c34fb2cba4a205f45ce4cc3a4918783d1e0d3
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-07-13 16:47:37 +0400

    Revert "#9 ContactModificationTest работает, но незавершен"
    This reverts commit bbecd77187a59c6355489d36aad6d38a5a16dbe3.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
 
 public class ContactModificationTest extends TestBase {
@@ -16,11 +16,9 @@ public class ContactModificationTest extends TestBase {
       app.getContactHelper().createContract(new ContactData("Petr", "Petrovich", "Petrov", "Sadovaya st.,2,204b", "999-999", "8-999-111-22-33", "email@mail.ru", "[none]"), true);
     }
     List<ContactData> before = app.getContactHelper().getContactList();
-    int i=before.get(before.size() - 1).getId();
     app.getContactHelper().selectContact(before.size() - 1);
-
     app.getContactHelper().editSelectedContacts();
-    ContactData contact = new ContactData("changedName", "changedPatronim", "changedSurname", "new address", "888-000-000", "777-77-22-22", "newEmail@mail.ru", null);
+    ContactData contact = new ContactData(before.get(before.size() - 1).getId(), "changedName", "changedPatronim", "changedSurname", "new address", "888-000-000", "777-77-22-22", "newEmail@mail.ru", null);
     app.getContactHelper().fillContactData(contact, false);
     app.getContactHelper().submitUpdateContact();
     app.getContactHelper().returnToHomePage();
@@ -28,20 +26,13 @@ public class ContactModificationTest extends TestBase {
     //проверка количества записей
     Assert.assertEquals(after.size(), before.size());
     //проверка списков с записями
-    int max = 0;
-    for (ContactData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    contact.setId(max);
     before.remove(before.size() - 1);
     before.add(contact);
     //сортировка списка
-   // Comparator<? super ContactData> byId = (Comparator<ContactData>) (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
-    //before.sort(byId);
-    //after.sort(byId);
+    Comparator<? super ContactData> byId = (Comparator<ContactData>) (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+    before.sort(byId);
+    after.sort(byId);
     //сравнение списков
-    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+    Assert.assertEquals(before,after);
   }
 }
