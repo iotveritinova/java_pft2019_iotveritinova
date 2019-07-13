041aa1e17e3bee22af0c756a254efda4481b516d
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-07-13 18:03:32 +0400

    #9 ContactCreationTest добавлено сравнение списков. Задание 9 завершено

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -1,9 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
 
-import org.testng.annotations.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
-import ru.stqa.pft.addressbook.model.*;
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactCreationTest extends TestBase {
 
@@ -11,10 +14,23 @@ public class ContactCreationTest extends TestBase {
   @Test
   public void testContactCreation() throws Exception {
     app.getNavigationHelper().gotoContactPage();
+    List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().addNewContact();
-    app.getContactHelper().fillContactData(new ContactData("Petr", "Petrovich", "Petrov", "Sadovaya st.,2,204b", "999-999", "8-999-111-22-33", "email@mail.ru","test1"),true);
+    ContactData contact = new ContactData("Petr", "Petrovich", "Petrov", "Sadovaya st.,2,204b", "999-999", "8-999-111-22-33", "email@mail.ru", "[none]");
+    app.getContactHelper().fillContactData(contact, true);
     app.getContactHelper().submitContactCreation();
     app.getContactHelper().returnToHomePage();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    //проверка количества записей
+    Assert.assertEquals(after.size(), before.size() + 1);
+    //проверка списков с записями
+    before.add(contact);
+    //сортировка списка
+    Comparator<? super ContactData> byId = (Comparator<ContactData>) (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    //сравнение списков
+    Assert.assertEquals(before, after);
 
   }
 
