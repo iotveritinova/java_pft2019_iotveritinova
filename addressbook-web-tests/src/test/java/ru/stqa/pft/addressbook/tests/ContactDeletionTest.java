55626288d251f0e056e185e129aeb3d3cfeeb2e6
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-07-13 17:52:38 +0400

    #9 ContactDeletionTest добавлено сравнение списков. TestBase переключен на Chrome, так как при большом количестве записей список after считывается до перехода на обновленную страницу контактов, что приводит к ошибкам

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
@@ -1,21 +1,38 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class ContactDeletionTest extends TestBase {
+
   @Test
   public void testContactDeletion() throws Exception {
     app.getNavigationHelper().gotoContactPage();
     if (!app.getContactHelper().isThereAContract()) {
       app.getContactHelper().createContract(new ContactData("Petr", "Petrovich", "Petrov", "Sadovaya st.,2,204b", "999-999", "8-999-111-22-33", "email@mail.ru", "[none]"), true);
     }
-    app.getContactHelper().selectContact(1);
+    List<ContactData> before = app.getContactHelper().getContactList();
+    app.getContactHelper().selectContact(before.size() - 1);
     app.getContactHelper().deleteSelectedContacts();
     app.getAlertHelper().isAlertPresent();
     app.getAlertHelper().acceptAlert();
     app.getContactHelper().returnToHomePage();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    //проверка количества записей
+    Assert.assertEquals(after.size(), before.size() - 1);
+    //проверка списков с записями
+    before.remove(before.size() - 1);
+    //сортировка списка
+    Comparator<? super ContactData> byId = (Comparator<ContactData>) (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    //сравнение списков
+    Assert.assertEquals(before, after);
   }
 }
 
