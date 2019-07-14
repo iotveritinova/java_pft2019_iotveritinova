0411ae6c9c7c016d377ad7117184b3fd18fb1746
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-07-14 11:15:34 +0400

    5.7. в тесты с группами добавлено кеширование

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/app/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/app/GroupHelper.java
@@ -49,6 +49,7 @@ public class GroupHelper extends BaseHelper {
     initGroupCreation();
     fillGroupForm(groupData);
     submitGroupCreation();
+    groupCache = null;
     returnToGroupPage();
   }
 
@@ -57,12 +58,14 @@ public class GroupHelper extends BaseHelper {
     initGroupUpdate();
     fillGroupForm(group);
     submitGroupUpdate();
+    groupCache = null;
     returnToGroupPage();
   }
 
   public void delete(GroupData group) {
     selectGroupById(group.getId());
     deleteSelectedGroups();
+    groupCache = null;
     returnToGroupPage();
   }
 
@@ -78,14 +81,19 @@ public class GroupHelper extends BaseHelper {
     return wd.findElements(By.name("selected[]")).size();
   }
 
+  private Groups groupCache = null;
+
   public Groups all() {
-    Groups groups = new Groups();
+    if (groupCache != null) {
+      return new Groups(groupCache);
+    }
+    groupCache = new Groups();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements) {
       String name = element.getText();
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-      groups.add(new GroupData().withId(id).withName(name));
+      groupCache.add(new GroupData().withId(id).withName(name));
     }
-    return groups;
+    return new Groups(groupCache);
   }
 }
