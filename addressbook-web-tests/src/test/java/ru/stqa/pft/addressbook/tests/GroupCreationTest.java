ad8fecae0f752034cdba192f3a4a4a32a80ed323
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-07-16 23:28:07 +0400

    6 блок. validGroups теперь работает с группами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -17,17 +17,16 @@ public class GroupCreationTest extends TestBase {
   @DataProvider
   public Iterator<Object[]> validGroups() {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[]{"test1", "header 1", "footer 1"});
-    list.add(new Object[]{"test2", "header 2", "footer 2"});
-    list.add(new Object[]{"test3", "header 3", "footer 3"});
+    list.add(new Object[]{new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
+    list.add(new Object[]{new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
+    list.add(new Object[]{new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
     return list.iterator();
   }
 
   @Test(dataProvider = "validGroups")
-  public void testGroupCreation(String name, String header, String footer) throws Exception {
+  public void testGroupCreation(GroupData group) throws Exception {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName(name).withHeader(header).withFooter(footer);
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
     Groups after = app.group().all();
