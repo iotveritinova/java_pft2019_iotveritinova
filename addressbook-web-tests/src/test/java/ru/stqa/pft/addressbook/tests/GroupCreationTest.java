d40dcba250860408ed556d3f66e5cb0c9b77bb9f
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-07-16 23:22:52 +0400

    6 блок. GroupCreationTest добавлен @DataProvider

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -1,22 +1,35 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase {
 
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{"test1", "header 1", "footer 1"});
+    list.add(new Object[]{"test2", "header 2", "footer 2"});
+    list.add(new Object[]{"test3", "header 3", "footer 3"});
+    return list.iterator();
+  }
 
-  @Test
-  public void testGroupCreation() throws Exception {
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(String name, String header, String footer) throws Exception {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test2");
+    GroupData group = new GroupData().withName(name).withHeader(header).withFooter(footer);
     app.group().create(group);
-    assertThat(app.group().count(), equalTo(before.size()+1));
+    assertThat(app.group().count(), equalTo(before.size() + 1));
     Groups after = app.group().all();
     assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
@@ -32,5 +45,4 @@ public class GroupCreationTest extends TestBase {
     Groups after = app.group().all();
     assertThat(after, equalTo(before));
   }
-
 }
