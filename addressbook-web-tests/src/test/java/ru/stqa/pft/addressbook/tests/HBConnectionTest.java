8a5a3fe0bdad02e0200c2820561aa394372f04f6
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-07-21 17:35:13 +0400

    7.3 HBConnectionTest перенастроен на ContactData

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HBConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HBConnectionTest.java
@@ -7,7 +7,7 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.util.List;
 
@@ -30,9 +30,9 @@ public class HBConnectionTest {
   public void testHBConnection() {
     Session session = sessionFactory.openSession();
     session.beginTransaction();
-    List<GroupData> result = session.createQuery("from GroupData").list();
-    for (GroupData group : result) {
-      System.out.println(group);
+    List<ContactData> result = session.createQuery("from ContactData").list();
+    for (ContactData contact : result) {
+      System.out.println(contact);
     }
     session.getTransaction().commit();
     session.close();
