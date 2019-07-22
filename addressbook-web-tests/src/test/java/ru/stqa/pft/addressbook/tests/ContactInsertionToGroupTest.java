8af54775c9fe0d62e3abe1c1c7851da4f6ad1363
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-07-22 04:20:42 +0400

    #16 testContactInsertionToGroup добавлена проверка групп, надо доработать

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactInsertionToGroupTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactInsertionToGroupTest.java
@@ -7,6 +7,9 @@ import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -42,7 +45,24 @@ public class ContactInsertionToGroupTest extends TestBase {
     assertThat(app.contact().count(), equalTo(before.size()));
     Contacts after = app.db().contacts();
     assertThat(after, equalTo(before.without(contact).withAdded(contact.inGroup(group))));
-    //verifyGroupListInUI();
+    //проверка групп
+    Set<Groups> befG = new HashSet<Groups>();
+    Set<Groups> afG = new HashSet<Groups>();
+    for (ContactData s : after) {
+      System.out.println(s);
+      System.out.println(s.getGroups());
+      afG.add(s.getGroups());
+    }
+    for (ContactData s : before) {
+      System.out.println(s);
+      System.out.println(s.getGroups());
+      if (s.getId() != contact.getId()) {
+        befG.add(s.getGroups());
+      } else {
+        befG.add(s.inGroup(group).getGroups());
+      }
+    }
+    assertThat(afG, equalTo(befG));
+    //after.stream().map((p) -> p.getGroups()).collect(Collectors.toSet()),
   }
-
 }
