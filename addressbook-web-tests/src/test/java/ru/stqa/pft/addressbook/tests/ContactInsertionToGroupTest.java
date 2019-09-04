636d5760b4ccc554ce580435a14b32add1bb5642
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-09-04 18:56:58 +0400

    #16 in progress

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactInsertionToGroupTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactInsertionToGroupTest.java
@@ -5,7 +5,6 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
-import ru.stqa.pft.addressbook.model.Groups;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -35,14 +34,28 @@ public class ContactInsertionToGroupTest extends TestBase {
     if (contact.getGroups() == contact.inGroup(group).getGroups()) {
       app.contact().removeFromGroup(contact, group);
     }
-    Groups groups = app.db().groups();
+    //Groups groups = app.db().groups();
     Contacts before = app.db().contacts();
+    ContactData contactBefore = getContact(contact, before);
     //adding contact to group
     app.contact().addToGroup(contact, group);
     assertThat(app.contact().count(), equalTo(before.size()));
     Contacts after = app.db().contacts();
-    assertThat(after, equalTo(before.without(contact).withAdded(contact.inGroup(group))));
+    ContactData contactAfter = getContact(contact, after);
     //проверка групп
-    assertThat(getGroupsListAsIs(after), equalTo(getGroupsListPlus(before, contact, group)));
+    //  assertThat(getGroupsListAsIs(after), equalTo(getGroupsListPlus(before, contact, group)));
+    System.out.println("summary log:");
+    System.out.println("groups of selected contact " + contact.getGroups());
+    System.out.println("groups of initial contact " + contactBefore.getGroups());
+    System.out.println("groups of expectedly changedcontact " + contactBefore.inGroup(group).getGroups());
+    System.out.println("groups of really changed contact " + contactAfter.getGroups());
+    //System.out.println(contactBefore.getGroups().size());
+    //System.out.println(contactAfter.getGroups().size());
+    //assertThat(contactBefore.getGroups().size(), equalTo(contactAfter.getGroups().size() - 1));
+    assertThat(contactBefore.inGroup(group).getGroups(), equalTo(contactAfter.getGroups()));
+  }
+
+  private ContactData getContact(ContactData contact, Contacts contacts) {
+    return contacts.stream().filter((c) -> c.equals(contact)).findFirst().get();
   }
 }
