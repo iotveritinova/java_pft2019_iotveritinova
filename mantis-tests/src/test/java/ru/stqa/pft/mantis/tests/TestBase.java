99a526c98e34663f4069749f4aec7ca17b78fb7a
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-09-04 22:28:49 +0400

    #17 добавлена структура проекта для Mantis TestBase+ApplicationManager

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.app.ApplicationManager;
+
+import java.io.IOException;
+
+public class TestBase {
+
+  protected static ApplicationManager app;
+
+  static {
+    try {
+      app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @BeforeSuite(alwaysRun = true)
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() throws Exception {
+    app.stop();
+  }
+}
