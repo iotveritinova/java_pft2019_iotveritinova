7f586b181e36a89c68ebcc8166d5a6766475fcdc
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-09-06 19:56:44 +0400

    №18 8.4 добавлена заготовка RegistrationTests, добавлена ленивая инициализация

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -13,8 +13,9 @@ import java.util.Properties;
 
 public class ApplicationManager {
   private final Properties properties;
-  public WebDriver wd;
+  private WebDriver wd;
   private String browser;
+  private RegistrationHelper registrationHelper;
 
   public ApplicationManager(String browser) throws IOException {
     this.browser = browser;
@@ -24,19 +25,14 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
-    }
-    wd.get(properties.getProperty("web.baseUrl"));
+
   }
 
 
   public void stop() {
-    wd.quit();
+    if (wd != null) {
+      wd.quit();
+    }
   }
 
   public HttpSession newSession() {
@@ -45,5 +41,26 @@ public class ApplicationManager {
 
   public String getProperty(String key) {
     return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public WebDriver getDriver() {
+    if (wd == null) {
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+      wd.get(properties.getProperty("web.baseUrl"));
+    }
+    return wd;
   }
 }
