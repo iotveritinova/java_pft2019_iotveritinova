9acf2d6da042dffe6eccb40c6be97d3ae95b1630
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-09-15 23:11:29 +0400

    8.5 создан FtpHelper

---
+++ mantis-tests/out/test/resources/config_inc.php
@@ -0,0 +1,12 @@
+<?php
+$g_hostname				= 'localhost';
+$g_db_username			= 'root';
+$g_db_password			= '';
+$g_database_name		= 'bugtracker';
+$g_db_type				= 'mysqli';
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = 'Gse45u1mmnNy3J/T0zaAybvT/6BQtaMML2CWIXQ18fo=';
+
+$g_signup_use_captcha	= OFF;
