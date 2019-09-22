a30feb684d1a302170d66701489ee9d1e480534b
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-09-22 22:50:48 +0400

    Revert "Revert "8.7 добавлен JamesHelper. Не отлажено""
    This reverts commit 4cf579c9bc886bcfde939fd66648c8afc5b76bba.

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -18,6 +18,7 @@ public class ApplicationManager {
   private RegistrationHelper registrationHelper;
   private FtpHelper ftp;
   private MailHelper mailHelper;
+  private JamesHelper jamesHelper;
 
   public ApplicationManager(String browser) throws IOException {
     this.browser = browser;
@@ -63,6 +64,12 @@ public class ApplicationManager {
       mailHelper = new MailHelper(this);
     }
     return mailHelper;
+  }
+  public JamesHelper james() {
+    if (jamesHelper == null) {
+      jamesHelper = new JamesHelper(this);
+    }
+    return jamesHelper;
   }
 
   public WebDriver getDriver() {
