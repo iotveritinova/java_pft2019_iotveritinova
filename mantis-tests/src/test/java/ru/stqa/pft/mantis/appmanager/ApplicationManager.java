4cf579c9bc886bcfde939fd66648c8afc5b76bba
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-09-22 22:42:04 +0400

    Revert "8.7 добавлен JamesHelper. Не отлажено"
    This reverts commit 43332a74dd1a2aba8a302ad03059cde74a59fd85.

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -18,7 +18,6 @@ public class ApplicationManager {
   private RegistrationHelper registrationHelper;
   private FtpHelper ftp;
   private MailHelper mailHelper;
-  private JamesHelper jamesHelper;
 
   public ApplicationManager(String browser) throws IOException {
     this.browser = browser;
@@ -64,12 +63,6 @@ public class ApplicationManager {
       mailHelper = new MailHelper(this);
     }
     return mailHelper;
-  }
-  public JamesHelper james() {
-    if (jamesHelper == null) {
-      jamesHelper = new JamesHelper(this);
-    }
-    return jamesHelper;
   }
 
   public WebDriver getDriver() {
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft.mantis.model.MailMessage;
@@ -10,21 +12,19 @@ import java.util.List;
 import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase {
-  //@BeforeMethod
+  @BeforeMethod
   public void startMailServer() {
     app.mail().start();
   }
 
   @Test
   public void testRegistration() throws IOException {
-    Long now = System.currentTimeMillis();
-    String user = String.format("user%s", now);
+    Long now=System.currentTimeMillis();
+    String user = String.format("user%s",now);
     String password = "password";
-    String email = String.format("user%s@localhost.localdomain", now);
-    app.james().createUser(user, password);
+    String email = String.format("user%s@localhost.localdomain",now);
     app.registration().start(user, email);
-    //List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
-    List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
+    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
     app.registration().finish(confirmationLink, password);
     assertTrue(app.newSession().login(user, password));
@@ -36,7 +36,7 @@ public class RegistrationTests extends TestBase {
     return regex.getText(mailMessage.text);
   }
 
-  // @AfterMethod(alwaysRun = true)
+  @AfterMethod(alwaysRun = true)
   public void stopMailServer() {
     app.mail().stop();
   }
