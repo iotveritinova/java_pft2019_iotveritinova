920e25e7367c5646ae5e1ed0a4f5a7e1514700b7
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-10-08 17:57:28 +0400

    testRegistration убрано использование james для удобства

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft.mantis.model.MailMessage;
@@ -11,7 +13,7 @@ import java.util.List;
 import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase {
-  //@BeforeMethod
+  @BeforeMethod
   public void startMailServer() {
     app.mail().start();
   }
@@ -22,10 +24,10 @@ public class RegistrationTests extends TestBase {
     String user = String.format("user%s", now);
     String password = "password";
     String email = String.format("user%s@localhost", now);
-    app.james().createUser(user, password);
+    //app.james().createUser(user, password);
     app.registration().start(user, email);
-    //List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
-    List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
+    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    //List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
     app.registration().finish(confirmationLink, password);
     assertTrue(app.newSession().login(user, password));
@@ -37,8 +39,8 @@ public class RegistrationTests extends TestBase {
     return regex.getText(mailMessage.text);
   }
 
-  // @AfterMethod(alwaysRun = true)
-  public void stopMailServer() {
+  @AfterMethod(alwaysRun = true)
+  private void stopMailServer() {
     app.mail().stop();
   }
 }
