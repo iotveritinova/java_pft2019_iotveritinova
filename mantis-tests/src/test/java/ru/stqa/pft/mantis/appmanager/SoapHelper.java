e46f22d170c4c874b015deebbbc16bd4ad72eee2
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2019-10-09 17:27:03 +0400

    lec 9. testCreateIssue added

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SoapHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SoapHelper.java
@@ -1,11 +1,11 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
-import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
-import biz.futureware.mantis.rpc.soap.client.ProjectData;
+import biz.futureware.mantis.rpc.soap.client.*;
+import ru.stqa.pft.mantis.model.Issue;
 import ru.stqa.pft.mantis.model.Project;
 
 import javax.xml.rpc.ServiceException;
+import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
@@ -13,16 +13,43 @@ import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class SoapHelper {
+public class SoapHelper extends HelperBase {
   private ApplicationManager app;
 
   public SoapHelper(ApplicationManager app) {
+    super(app);
     this.app = app;
   }
 
   public Set<Project> getProjects() throws MalformedURLException, RemoteException, ServiceException {
-    MantisConnectPortType mc = new MantisConnectLocator().getMantisConnectPort(new URL("http://localhost/mantisbt-2.22.0/api/soap/mantisconnect.php"));
+    MantisConnectPortType mc = getMantisConnect();
     ProjectData[] projects = mc.mc_projects_get_user_accessible(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"));
     return Arrays.asList(projects).stream().map((p) -> new Project().withId(p.getId().intValue()).withName(p.getName())).collect(Collectors.toSet());
+  }
+
+  private MantisConnectPortType getMantisConnect() throws ServiceException, MalformedURLException {
+    return new MantisConnectLocator().getMantisConnectPort(new URL(app.getProperty("web.baseUrl") + "/api/soap/mantisconnect.php"));
+  }
+
+  public Issue addIssue(Issue issue) throws MalformedURLException, ServiceException, RemoteException {
+    MantisConnectPortType mc = getMantisConnect();
+    String admLogin = app.getProperty("web.adminLogin");
+    String admPass = app.getProperty("web.adminPassword");
+    String[] categories = mc.mc_project_get_categories(admLogin, admPass, BigInteger.valueOf(issue.getProject().getId()));
+    IssueData issueData = new IssueData();
+    issueData.setSummary(issue.getSummary());
+    issueData.setDescription(issue.getDescription());
+    issueData.setProject(new ObjectRef(BigInteger.valueOf(issue.getProject().getId()), issue.getProject().getName()));
+    issueData.setCategory(categories[0]);
+    BigInteger issueId=mc.mc_issue_add(admLogin, admPass, issueData);
+    IssueData createdIssueData = mc.mc_issue_get(admLogin, admPass, issueId);
+    return new Issue()
+            .withId(createdIssueData.getId().intValue())
+            .withSummary(createdIssueData.getSummary())
+            .withDescription(createdIssueData.getDescription())
+            .withProject(new Project()
+                    .withId(createdIssueData.getProject().getId().intValue())
+                    .withName(createdIssueData.getProject().getName())
+            );
   }
 }
